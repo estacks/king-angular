c608204d66854cd8e6661a1b468a4350b57e52b6
estacks <eric@king.rocks>
2018-05-18 14:05:54 -0400

    Can now view posts.

--- src/app/services/wp.service.ts
+++ src/app/services/wp.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,10 +8,14 @@ import { environment } from 'src/environments/environment';
 })
 export class WpService {
   root: string = environment.url + 'wp/v2/';
+  settings: Subject<any> = new Subject();
 
   constructor(
     private http: HttpClient
   ) {
+    this.get('settings').subscribe((res) => {
+      this.settings.next(res);
+    }, () => {});
   }
 
   get(url: string, options?) {
