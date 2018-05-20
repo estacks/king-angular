7d55984a86185830fb01530501f3e1826c450b94
estacks <eric@king.rocks>
2018-05-19 22:03:15 -0400

    Added Typed.js for front screen typing effects

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import * as Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterContentInit {
   pageText: string;
+  h1text: any;
+  h2text: any;
 
   context = {
     test: ['One', 'Two', 'Butt']
@@ -20,6 +23,21 @@ export class HomeComponent implements OnInit {
     this.title.setTitle('Welcome');
     this.route.data.subscribe((data: { pages: Array<any> }) => {
       this.pageText = data.pages[0].content.rendered;
+    });
+  }
+
+  ngAfterContentInit() {
+    let title = new Typed('#main-title-display', {
+      stringsElement: '#main-title-text',
+      typeSpeed: 20,
+      showCursor: false
+    });
+
+    var subtitle = new Typed('#sub-title-display', {
+      stringsElement: '#sub-title-text',
+      startDelay: 2000,
+      typeSpeed: 20,
+      showCursor: false
     });
   }
 }
